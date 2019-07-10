6709cbcd09e412baeed399da864d7ed7a5ae989f
CodeRepository19 <coderepository2019@gmail.com>
2019-07-10 14:09:51 +0530

    UI Project Creation In MVC

---
+++ EbookStore/EbookInfraData/Context/ebooklibraryDBcontext.cs
@@ -0,0 +1,15 @@
+﻿using EbookDomain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EbookInfraData.Context
+{
+    public class ebooklibraryDBcontext: DbContext
+    {
+        public ebooklibraryDBcontext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
