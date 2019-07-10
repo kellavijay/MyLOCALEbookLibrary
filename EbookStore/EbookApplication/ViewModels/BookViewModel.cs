03b8f4b86b92855a0b6d9fea7b6fc379b11b2d96
CodeRepository19 <coderepository2019@gmail.com>
2019-07-10 15:21:06 +0530

    Application Layer comitted by vijay

---
+++ EbookStore/EbookApplication/ViewModels/BookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EbookDomain.Models;
+
+namespace EbookApplication.ViewModels
+{
+    public class BookViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
---
+++ EbookStore/EbookApplication/interfaces/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using EbookApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EbookApplication.interfaces
+{
+    public interface IBookService
+    {
+        IEnumerable<BookViewModel> GetBooks();
+    }
+}
