5613272d43db724e45fabc14dc702c4e0534e797
Administrator <Administrator@CITADEL-9>
2019-07-11 15:19:45 +0530

    Implement curd operation

--- EbookStore/EbookDomain/Models/Book.cs
+++ EbookStore/EbookDomain/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -12,6 +13,7 @@ namespace EbookDomain.Models
         public string BookName { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public List<IFormFile> Image { get; set; }
         public int TechnologyId { get; set; }
         public Technology technology { get; set; }
     }
--- EbookStore/EbookUI/Controllers/BooksController.cs
+++ EbookStore/EbookUI/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EbookDomain.Models;
 using EbookInfraData.Context;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,46 @@ namespace EbookUI.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Books.ToListAsync());
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BookId,BookName,Description,ImageUrl,TechnologyId")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
         }
 
     }
