d6de8085960cd26c9cfe4f991a05132bdb2c2e64
Administrator <Administrator@CITADEL-9>
2019-07-11 14:07:56 +0530

    Add Book Controller and ebookdbcontext

--- EbookStore/EbookInfraData/Context/ebooklibraryDBcontext.cs
+++ EbookStore/EbookInfraData/Context/ebooklibraryDBcontext.cs
@@ -3,13 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EbookInfraData.Context
 {
-    public class ebooklibraryDBcontext: DbContext
+    public class ebooklibraryDBcontext : DbContext
     {
-        public ebooklibraryDBcontext(DbContextOptions options):base(options)
+        public ebooklibraryDBcontext(DbContextOptions options) : base(options)
         {
 
         }
 
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
     }
 }
--- EbookStore/EbookUI/Controllers/BooksController.cs
+++ EbookStore/EbookUI/Controllers/BooksController.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EbookInfraData.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EbookUI.Controllers
 {
     public class BooksController : Controller
     {
-        public IActionResult Index()
+        private readonly ebooklibraryDBcontext _context;
+
+        public BooksController(ebooklibraryDBcontext context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+
     }
 }
